cf03017ea10e8b1e4a813784ed3c725a16adeb4b
wisdomhove <hovewisdom@gmail.com>
2022-10-16 11:16:07 +0200

    Add project files.

---
+++ EmployeeBL.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DepencyInjectionExample
+{
+    public class EmployeeBL
+    {
+        public EmployeeDAL employeeDAL;
+        public List<Employee> GetAllEmployees()
+        {
+            employeeDAL = new EmployeeDAL();
+            return employeeDAL.SelectAllEmployees();
+        }
+    }
+}
